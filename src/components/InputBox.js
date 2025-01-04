63e3615fd83262fd44dda46faf96a2ad3455ca88
namizz <139370392+namizz@users.noreply.github.com>
2025-01-04 14:53:18 +0300

    db

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,35 +1,3 @@
-// import "../components/inputbox.css";
-
-// const InputBox = (props) => {
-//   return (
-//     <div className="input" id={`${props.name}Box`}>
-//       <p className="boxname">
-//         {props.p}
-//         {props.display ? <span id="required">*</span> : ""}
-//       </p>
-//       <input
-//         className="inputbox"
-//         type={props.type || "text"}
-//         id={`${props.name}Text`}
-//         name={props.name}
-//         value={props.value}
-//         onChange={props.change}
-//       />
-//       <p style={{ display: props.display || "none" }} className="message">
-//         ⚠️ Please enter {props.name}
-//       </p>
-//       <p
-//         style={props.msg == "" ? { display: "none" } : { display: "flex" }}
-//         className="message"
-//       >
-//         {props.msg}
-//       </p>
-//     </div>
-//   );
-// };
-
-// export default InputBox;
-
 const Name = ({ props }) => {
   return (
     <p className="text-sm text-gray-700 font-[600]">
